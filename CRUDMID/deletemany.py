a5af0b0195c50f7c386fb9f2e58c1411789193a8
kowshikavula <158231420+kowshikavula@users.noreply.github.com>
2024-02-21 12:58:22 -0500

    deletemany.py

---
+++ CRUDMID/deletemany.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://kowshikavula14:Smileysmiley183@cluster0.prsfy7f.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    # Send a ping to confirm a successful connection
+    client.admin.command('ping')
+
+    # Get reference to the 'employee_management' database
+    db = client.employee_management
+
+    # Get reference to the 'employees' collection
+    employees_collection = db.employees
+
+    # Define the filter to identify the documents to delete
+    filter_criteria = {"department": "Engineering"}  # Example filter criteria
+
+    # Use delete_many() to delete the documents
+    result = employees_collection.delete_many(filter_criteria)
+
+    # Print information about the delete operation
+    print("Deleted %d documents" % result.deleted_count)
+except Exception as e:
+    print(e)
+finally:
+    # Close the connection
+    client.close()
